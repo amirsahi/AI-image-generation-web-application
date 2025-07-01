df49ee07c2d32cbb16fe23f1a7c655c4888ed66f
amirsahi <amirhossein.sahraee@gmail.com>
2025-07-01 16:27:27 +0200

    initial commit

--- ml.py
+++ ml.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import torch
 from diffusers import StableDiffusionPipeline
 from PIL.Image import Image
+from IPython.display import display
 
 token_path = Path("token.txt")
 token = token_path.read_text().strip()
@@ -18,7 +19,29 @@ pipe.to("cpu")   #if GPU is available, change to "cuda"
 
 prompt = "a photograph of an astronaut riding a horse"
 
-image = pipe(prompt, num_inference_steps=50).images[0]
+#image = pipe(prompt, num_inference_steps=50).images[0]
 
-image.save("astronaut_horse.png")      
-print("done → astronaut_horse.png")
+
+def obtain_image(
+    prompt: str,
+    *,
+    seed: int | None = None,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5,
+) -> Image:
+    # pick the right device automatically
+    device = pipe.device.type           # "cpu", "cuda" if GPU is avalable
+    generator = None if seed is None else torch.Generator(device).manual_seed(seed)
+
+    print(f"Using device: {device}")
+    image: Image = pipe(
+        prompt,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        generator=generator,
+    ).images[0]
+    return image
+
+
+image = obtain_image(prompt, num_inference_steps=5, seed=1024)
+display(image)
